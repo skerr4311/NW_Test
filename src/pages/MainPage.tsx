88c4d248eb71d6db3d52a91cad4b79bc2153e6a0
Steve <sker078@aucklanduni.ac.nz>
2022-05-11 09:02:27 +1200

    created main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage:React.FC = () => {
+    return(
+        <div>
+            main page not set up...
+        </div>
+    );
+}
+
+export default MainPage;
